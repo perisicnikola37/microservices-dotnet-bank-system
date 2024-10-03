b25d563438ba89a8a78cf717fd9a602b2dad2a3c
Nikola <nikola.perisic@vegait.rs>
2024-10-03 14:59:36 +0200

    Finish Ocelot API Gateway project

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,6 +1,29 @@
+using AspNetCoreRateLimit;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
